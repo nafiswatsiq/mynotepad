23d95e860a073c2f60b8448fbb05e9ccff8841b5
nafiswatsiq <amrullohnafis@gmail.com>
2022-09-05 21:53:38 +0700

    chore: process calendar

--- proses/calendar.php
+++ proses/calendar.php
@@ -17,4 +17,34 @@ echo $desc;
 echo '<br>';
 echo $color;
 echo '<br>';
-echo $icon;
+echo $icon; 
+echo '<br>';
+$str = $date;
+$explode = explode(" - ",$str);
+
+// $date_start = $explode[0];
+$date_start = date('Y-m-d H:i:s');
+$date_end = $explode[1];
+
+$ex_end = explode("/", $date_end);
+// echo $date_end;
+echo implode("-", $ex_end);
+
+// $save = mysqli_query($conn, "INSERT INTO `calendar`(`id`, 
+//                                                     `id_user`, 
+//                                                     `name`, 
+//                                                     `description`, 
+//                                                     `color`, 
+//                                                     `icon`, 
+//                                                     `start`, 
+//                                                     `end`
+//                                                     ) VALUES (
+//                                                     '',
+//                                                     '$id_user',
+//                                                     '$name',
+//                                                     '$desc',
+//                                                     '$color',
+//                                                     '$icon',
+//                                                     '$date_start',
+//                                                     '$date_end'
+//                                                     )");
